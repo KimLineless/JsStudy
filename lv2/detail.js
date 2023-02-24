6c5d8b9a76660018864fc9062b6195b62d1a8993
김무선 <96815572+KimLineless@users.noreply.github.com>
2023-02-24 19:01:10 +0900

    study

---
+++ lv2/detail.js
@@ -0,0 +1,26 @@
+let tab = document.querySelectorAll('.tab-button');
+let con = document.querySelectorAll('.tab-content');
+
+// 반복문
+// for (let i = 0; i < tab.length; i++) {
+//   tab[i].addEventListener('click', () => {
+//     tabc(i);
+//   });
+// }
+
+// 이벤트버블링 응용
+
+document.querySelector('.list').addEventListener('click', (e) => {
+  tabc(e.target.dataset.id);
+});
+
+function tabc(j) {
+  tab[0].classList.remove('orange');
+  tab[1].classList.remove('orange');
+  tab[2].classList.remove('orange');
+  tab[j].classList.add('orange');
+  con[0].classList.remove('show');
+  con[1].classList.remove('show');
+  con[2].classList.remove('show');
+  con[j].classList.add('show');
+}
